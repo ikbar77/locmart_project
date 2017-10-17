441619ec79016d66f0ce6f659bb2f394b2bc3888
ikbar77 <ikbarmaulana77@gmail.com>
2017-10-17 08:39:51 +0700

    introslider & splash screen done

--- app/src/main/java/com/example/ikbar/locmart/IndomartProduct.java
+++ app/src/main/java/com/example/ikbar/locmart/IndomartProduct.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IKBAR on 10/11/2017.
@@ -20,6 +26,43 @@ public class IndomartProduct extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.indomart_product, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner_indomart);
+
+        List<String> categories = new ArrayList<>();
+        categories.add("Food");
+        categories.add("Drink");
+        categories.add("Cigarette");
+        categories.add("Healthy & Beauty");
+        categories.add("Household Appliance");
+        categories.add("Toys");
+        categories.add("Stationary");
+        categories.add("Mom & Kids");
+
+        //creat adapter spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, categories);
+
+        //dropdown layout style
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+
         return view;
     }
 }
